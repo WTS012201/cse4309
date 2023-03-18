ffee2f07b9039c2a011c1a4d2b039ecc330f6fc3
Will <WTS012201@gmail.com>
2023-03-18 16:37:37 -0500

    random sample assignment3

--- assignment3/MLChoice.py
+++ assignment3/MLChoice.py
@@ -11,7 +11,8 @@ import numpy as np
 from collections import Counter
 import pandas as pd
 from sklearn.utils import shuffle
-from sklearn.neighbors import KNeighborsClassifier as KNN
+from sklearn.neighbors import KNeighborsClassifier as skl_knn
+from sklearn.svm import SVC as skl_svm
 
 BANKNOTE_DATA_FILE = "data_banknote_authentication.txt"
 SONAR_DATA_FILE = "sonar.txt"
@@ -22,6 +23,15 @@ class MLChoice:
         self.dataset = dataset
         self.train, self.test = self.load_data()
         self.choice = choice
+
+        if choice == "knn":
+            k = 5
+            self.predict = lambda x: self.knn(x, k)
+            self.skl = skl_knn(n_neighbors=k)
+        elif choice == "svm":
+            self.predict = self.svm
+            self.skl = skl_svm(kernel="linear", probability=True)
+
         self.result = self.classify()
         self.log_results()
 
@@ -47,43 +57,59 @@ class MLChoice:
             else:
                 test_set[i[-1]] = [i[:-1].astype(float)]
 
+        self.X, self.Y = [], []
+        for group in train_set:
+            for data in train_set[group]:
+                self.X.append(data)
+                self.Y.append(group)
+
         return train_set, test_set
 
     def log_results(self):
+        ds = "Banke Note" if self.dataset == "banknote" else "Sonar"
         print(
-            f"DataSet: {self.dataset}\n\n" +
+            f"DataSet: {ds}\n\n" +
             f"Machine Learning Algorithm Chosen: {self.choice.upper()}\n\n" +
-            f"Accuracy of Training (Scratch): {self.accuracy}\n\n" +
-            f"Accuracy of ScikitLearn Function: {self.skl_accuracy}\n\n"
+            f"Accuracy of Training (Scratch): {self.accuracy:.2f}%\n\n" +
+            f"Accuracy of ScikitLearn Function: {self.skl_accuracy:.2f}%\n"
         )
 
-        return
+        sample, prediction, actual = self.random_test_sample()
+        print(
+            f"Prediction Point: {np.array(sample)}\n" +
+            f"Predicted Class: {prediction}\n" +
+            f"Actual class: {actual}\n"
+        )
+
+    def random_test_sample(self):
+        groups = [*self.test.keys()]
+        sample_group = groups[np.random.randint(0, len(groups))]
+        sample_idx = np.random.randint(0, len(self.test[sample_group]))
+        sample = self.test[sample_group][sample_idx]
+        prediction = self.predict(sample)
+
+        return sample, prediction, sample_group
 
     def classify(self):
         skl_correct, correct, total = 0, 0, 0
-        skl_knn = KNN(n_neighbors=5)
-        X, y = [], []
-        for group in self.test:
-            for data in self.test[group]:
-                X.append(data)
-                y.append(group)
-        skl_knn.fit(X, y)
+        self.skl.fit(self.X, self.Y)
 
         for group in self.test:
             for data in self.test[group]:
-                vote = self.knn(self.train, data, k=5)
-                skl_vote = skl_knn.predict([data])
+                vote = self.predict(data)
+                skl_vote = self.skl.predict([data])
                 if group == vote:
                     correct += 1
                 if group == skl_vote:
                     skl_correct += 1
                 total += 1
 
-        self.accuracy = correct / total
-        self.skl_accuracy = skl_correct / total
+        self.accuracy = round(correct / total, 4) * 100
+        self.skl_accuracy = round(skl_correct / total, 4) * 100
         return None
 
-    def knn(self, data, predict, k=5):
+    def knn(self, predict, k):
+        data = self.train
         if len(data) >= k:
             warnings.warn('K is set to a value less than total voting groups!')
 
@@ -93,14 +119,12 @@ class MLChoice:
                 euclidean_distance = np.linalg.norm(
                     np.array(features) - np.array(predict))
                 distances.append([euclidean_distance, group])
-        # print(distances)
         votes = [i[1] for i in sorted(distances)[:k]]
-        # print(votes)
         vote_result = Counter(votes).most_common(1)[0][0]
         return vote_result
 
-    def svm(self):
-        pass
+    def svm(self, predict):
+        return 0
 
 
 def main():
